4a415a72c5403c2f862df3bbb009e9f538d32337
pab03005 <patrickabarton@gmail.com>
2019-01-26 18:58:47 -0500

    update

--- interactive-form-v1 2/js/app.js
+++ interactive-form-v1 2/js/app.js
@@ -19,3 +19,41 @@ document.getElementById("title").addEventListener('change', () => {
       otherTitleText.addEventListener('focus', ()=> {otherTitleText.value=''})
     }
 })
+
+
+let xx = document.getElementById('design');
+let shirtDesign = document.getElementById('design').children;
+let shirtColor = document.getElementById('color').children;
+document.getElementById('design').parentNode.addEventListener('change', (e) => {
+  if (e.target.selectedIndex === 1 ) {
+    shirtColor[0].setAttribute('selected', 'selected')
+    shirtColor[0].style.display = 'block'
+    shirtColor[1].style.display = 'block'
+    shirtColor[2].style.display = 'block'
+    shirtColor[3].style.display = 'none'
+    shirtColor[4].style.display = 'none'
+    shirtColor[5].style.display = 'none'
+}
+  if (e.target.selectedIndex === 2 ) {
+    shirtColor[3].setAttribute('selected', 'selected')
+    shirtColor[3].style.display = 'block'
+    shirtColor[4].style.display = 'block'
+    shirtColor[5].style.display = 'block'
+    shirtColor[0].style.display = 'none'
+    shirtColor[1].style.display = 'none'
+    shirtColor[2].style.display = 'none'
+}
+})
+
+const $confEntry = $("input[name='all']");
+const $jsFrameworks = $("input[name = 'js-frameworks']");
+$jsFrameworks.attr('disabled', true)
+/*
+<label><input type="checkbox" name="all"> Main Conference — $200</label>
+<label><input type="checkbox" name="js-frameworks"> JavaScript Frameworks Workshop — Tuesday 9am-12pm, $100</label>
+<label><input type="checkbox" name="js-libs"> JavaScript Libraries Workshop — Tuesday 1pm-4pm, $100</label>
+<label><input type="checkbox" name="express"> Express Workshop — Tuesday 9am-12pm, $100</label>
+<label><input type="checkbox" name="node"> Node.js Workshop — Tuesday 1pm-4pm, $100</label>
+<label><input type="checkbox" name="build-tools"> Build tools Workshop — Wednesday 9am-12pm, $100</label>
+<label><input typ
+*/
