1e74594e8fcee504bc1be21d09822bafe69781ee
pab03005 <patrickabarton@gmail.com>
2019-02-05 21:21:11 -0500

    final updates

--- interactive-form-v1 2/js/app.js
+++ interactive-form-v1 2/js/app.js
@@ -1,29 +1,30 @@
+//Treehouse FS JS Project #3, interactive form.
+
+//some basic global variables
 const nameForm = document.getElementById('name');
 const emailForm = document.getElementById('mail');
-nameForm.focus();
-
-//global variables
 let userTitle;
 const otherTitleText = document.getElementById('other_title');
+
+//focus on name input
+nameForm.focus();
 
 //hide 'other title' text input
 otherTitleText.style.display = 'none';
 
-//upon selecting 'other', unhide text input
+//upon selecting 'other', unhide text input, on click label hides, out focus appears
 document.getElementById("title").addEventListener('change', () => {
   otherTitleText.style.display = 'none'
   let x = document.getElementById("title").selectedIndex;
   userTitle = document.getElementsByTagName("option")[x].value;
     if (userTitle === 'other')
       { otherTitleText.style.display = 'block';
-      otherTitleText.addEventListener('focus', ()=> {otherTitleText.value=''})
-
+      otherTitleText.addEventListener('focus', ()=> {otherTitleText.value=''
+      })
     }
+  })
 
-})
-
-
-let xx = document.getElementById('design');
+//depending on theme, hide/show theme-specific colors by manipulating/selecting by index of options.
 let shirtDesign = document.getElementById('design').children;
 let shirtColor = document.getElementById('color').children;
 document.getElementById('design').parentNode.addEventListener('change', (e) => {
@@ -35,7 +36,7 @@ document.getElementById('design').parentNode.addEventListener('change', (e) => {
     shirtColor[3].style.display = 'none'
     shirtColor[4].style.display = 'none'
     shirtColor[5].style.display = 'none'
-}
+  }
   if (e.target.selectedIndex === 2 ) {
     shirtColor[3].setAttribute('selected', 'selected')
     shirtColor[3].style.display = 'block'
@@ -44,9 +45,11 @@ document.getElementById('design').parentNode.addEventListener('change', (e) => {
     shirtColor[0].style.display = 'none'
     shirtColor[1].style.display = 'none'
     shirtColor[2].style.display = 'none'
-}
+  }
 })
 
+//global variables used to select/maniuplate activites are declared
+//JQuery used here to select sctivity checkboxes
 const $all = $("input[name = 'all']");
 const $confEntry = $("input[name='all']");
 const $jsFrameworks = $("input[name = 'js-frameworks']");
@@ -61,6 +64,9 @@ let $actChecks = $(".activities");
 $actChecks.append($newP);
 let $actCntr = 0;
 
+//upon a 'change' to each activity, check if checkbox is checked
+//if checked, add cost & count # of Activities, if unchecked, erase cost and count of Activities
+//this is done for all available activities include 'all'/main conference
 $all.change(function() {
   if (this.checked) {
     $total += 200;
@@ -146,6 +152,8 @@ $buildtools.change (function() {
     $newP.text("Total: $" + $total);
   });
 
+//global variables used to select and manipulate payment option details are declared here
+
 const payDetails =  document.querySelector('#payment')
 const pp = document.getElementsByTagName('p')[1]
 const btc = document.getElementsByTagName('p')[2]
@@ -153,24 +161,60 @@ const cc = document.getElementById('credit-card')
 const all = document.getElementById('credit-card').parentNode.children
 const submitButton = document.getElementsByTagName('button')[0];
 
+//hies bitcoin and paypal messages & select CC as default
 btc.style.display = 'none';
 pp.style.display = 'none';
 payDetails[1].selected = 'selected';
 
+//depending on chose payment option (identified by selected index), show/hide instructions
+
+payDetails.addEventListener('change', (e) => {
+  if (e.target.selectedIndex === 1 )
+    {cc.style.display = 'block';
+      btc.style.display = 'none';
+      pp.style.display = 'none';
+  ccnum.focus()
+    }
+
+  if (e.target.selectedIndex === 2 )
+    {pp.style.display = 'block';
+      btc.style.display = 'none';
+      cc.style.display = 'none';}
+
+  if (e.target.selectedIndex === 3 )
+    {btc.style.display = 'block';
+      pp.style.display = 'none';
+      cc.style.display = 'none';
+      submitButton.disabled = false}
+
+  if (e.target.selectedIndex === 0 )
+    {submitButton.disabled = true;
+      btc.style.display = 'none';
+      pp.style.display = 'none';
+      cc.style.display = 'none';}
+
+    })
+
+//global variables to select & manipulate credit card text inputs are declared here
 const ccnum = document.querySelector('#cc-num')
 const cczip = document.querySelector('#zip');
 const cccvv = document.querySelector('#cvv')
+//RegEx's for each CC form field
 const emailRegEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 const ccnumRegEx = /[0-9]{13,16}/;
 const cczipRegEx = /[0-9]{5}/;
 const cccvvRegEx = /[0-9]{2}/;
+
+/*if CC is method of payment, check if each CC field is completed AND in proper format.
+if empty, error messsage says so; if not empty but not proper format, error message will
+alert user.  The form will not submit unless CC#, Zip, or CVV are present & proper format*/
 
 submitButton.addEventListener('click', (e) => {
   if (payDetails.selectedIndex === 1) {
 
     if (ccnumRegEx.test(ccnum.value) === false) {
           e.preventDefault();
-          if (ccnum.value === '') {
+          if (ccnum.value === '' || ccnum.value === "CC Number is required") {
             ccnum.value = 'CC Number is required';
             ccnum.style.color = 'red';
             ccnum.addEventListener('focus', () => {
@@ -187,7 +231,7 @@ submitButton.addEventListener('click', (e) => {
 
     if (cczipRegEx.test(cczip.value) === false) {
           e.preventDefault();
-          if (cczip.value === '') {
+          if (cczip.value === '' || cczip.value === "ZIP is required") {
             cczip.value = 'ZIP is required';
             cczip.style.color = 'red';
             cczip.addEventListener('focus', () => {
@@ -204,14 +248,14 @@ submitButton.addEventListener('click', (e) => {
 
     if (cccvvRegEx.test(cccvv.value) === false) {
           e.preventDefault();
-          if (cccvv.value === '') {
+          if (cccvv.value === '' || cccvv.value === "CVV is required") {
               cccvv.value = 'CVV is required';
               cccvv.style.color = 'red';
               cccvv.addEventListener('focus', () => {
                 cccvv.value= '';
               })
             }
-          else  {cccvv.value = 'CVV 3 digits';
+          else {cccvv.value = 'CVV 3 digits';
               cccvv.style.color = 'red';
               cccvv.addEventListener('focus', () => {
                   cccvv.value= '';
@@ -223,39 +267,15 @@ submitButton.addEventListener('click', (e) => {
   )
 
 
-  payDetails.addEventListener('change', (e) => {
-    if (e.target.selectedIndex === 1 )
-      {cc.style.display = 'block';
-        btc.style.display = 'none';
-        pp.style.display = 'none';
-    ccnum.focus()
-      }
 
-    if (e.target.selectedIndex === 2 )
-      {pp.style.display = 'block';
-        btc.style.display = 'none';
-        cc.style.display = 'none';}
-
-    if (e.target.selectedIndex === 3 )
-      {btc.style.display = 'block';
-        pp.style.display = 'none';
-        cc.style.display = 'none';
-        submitButton.disabled = false}
-
-    if (e.target.selectedIndex === 0 )
-      {submitButton.disabled = true;
-        btc.style.display = 'none';
-        pp.style.display = 'none';
-        cc.style.display = 'none';}
-
-      })
-
-
+//global variable for name/email verification declared
 const label = nameForm.parentNode.children;
 
-
+/*upon click of submit button, checks to see if name field is blank. If blank
+prompts error messages and will not submit form. If name field completed, allows form
+to be submitted*/
 submitButton.addEventListener('click', (e) => {
-if (nameForm.value === '') {
+if (nameForm.value === '' || nameForm.value === 'Name is a required input') {
     e.preventDefault();
     nameForm.value = 'Name is a required input';
     nameForm.style.color = 'red'
@@ -264,16 +284,16 @@ if (nameForm.value === '') {
         nameForm.style.color = 'black';
       })
   }
-    else {
-      nameForm.addEventListener('focus', () => {
-          nameForm.value= '';
+    else {nameForm.addEventListener('focus', () => {
           nameForm.style.color = 'black';
         })
       }
     }
   )
 
-
+  /*upon click of submit button, checks to see if 1) email field is blank, or 2) email is
+  not properly formatted. If blank or improper format, prompts error messages and will
+  not submit form. If name field completed & formatted, allows form to be submitted*/
 
 submitButton.addEventListener('click', (e) => {
 if (emailRegEx.test(emailForm.value) === false) {
@@ -287,14 +307,17 @@ if (emailRegEx.test(emailForm.value) === false) {
   }
     else {
       emailForm.addEventListener('focus', () => {
-          emailForm.value= '';
           emailForm.style.color = 'black';
         })
       }
     }
   )
 
+//global variable to create span element
 let actAlert = document.createElement('SPAN');
+
+/*upon click of submit button, checks to see at least 1 activity chosen. If not, console.error
+message prompted and form does not submit;*/
 
 submitButton.addEventListener('click', (e) => {
 if ($actCntr === 0) {
